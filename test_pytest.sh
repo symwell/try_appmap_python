a915172cfe93d0c017625bdf2da08c1c5c59361d
symwell <111290954+symwell@users.noreply.github.com>
2022-10-07 09:54:10 -0400

    add

--- test_pytest.sh
+++ test_pytest.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#export APPMAP=true
+#export APPMAP_RECORD_UNITTEST=true
+#export APPMAP=false
 export PYTHONPATH=$HOME/src/appmap-python
 pytest -s -v program2_test.py
--- test_unittest.sh
+++ test_unittest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PYTHONPATH=$HOME/src/appmap-python
 #export APPMAP=true
 #export APPMAP_RECORD_UNITTEST=true
 #export APPMAP=false
+export PYTHONPATH=$HOME/src/appmap-python
 python3 -m appmap.unittest program_test.py
