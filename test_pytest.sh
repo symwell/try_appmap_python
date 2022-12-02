51bcccdb15416efcfd6f2c0b91ddf79b1288a838
symwell <111290954+symwell@users.noreply.github.com>
2022-12-02 13:25:57 -0500

    show before and after files

--- test_pytest.sh
+++ test_pytest.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-rm -rf tmp/appmap
+APPMAP_DIR=tmp/appmap
+rm -rf $APPMAP_DIR
+echo AppMap files before
+echo -------------------------------------------------------------------------
+find $APPMAP_DIR
+echo -------------------------------------------------------------------------
 
 #export APPMAP=true
 #export APPMAP_RECORD_UNITTEST=true
 #export APPMAP=false
 #export PYTHONPATH=$HOME/src/appmap-python:$PWD/init
 APPMAP=true pytest -s -v program_pytest.py
+
+echo AppMap files after
+echo -------------------------------------------------------------------------
+find $APPMAP_DIR
+echo -------------------------------------------------------------------------
--- test_unittest.sh
+++ test_unittest.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-rm -rf tmp/appmap
+APPMAP_DIR=tmp/appmap
+rm -rf $APPMAP_DIR
+echo AppMap files before
+echo -------------------------------------------------------------------------
+find $APPMAP_DIR
+echo -------------------------------------------------------------------------
 
 #export APPMAP=true
 #export APPMAP_RECORD_UNITTEST=true
 #export APPMAP=false
 #export PYTHONPATH=$HOME/src/appmap-python:$PWD/init
 APPMAP=true python3 -m unittest program_unittest.py
+
+echo AppMap files after
+echo -------------------------------------------------------------------------
+find $APPMAP_DIR
+echo -------------------------------------------------------------------------
