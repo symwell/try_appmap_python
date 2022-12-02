70e580e2025dfa8d24dbed5421717a84b9293e81
symwell <111290954+symwell@users.noreply.github.com>
2022-12-02 13:22:10 -0500

    more in script

--- test_pytest.sh
+++ test_pytest.sh
@@ -5,5 +5,5 @@ rm -rf tmp/appmap
 #export APPMAP=true
 #export APPMAP_RECORD_UNITTEST=true
 #export APPMAP=false
-#export PYTHONPATH=$HOME/src/appmap-python
+#export PYTHONPATH=$HOME/src/appmap-python:$PWD/init
 APPMAP=true pytest -s -v program_pytest.py
--- test_unittest.sh
+++ test_unittest.sh
@@ -5,5 +5,5 @@ rm -rf tmp/appmap
 #export APPMAP=true
 #export APPMAP_RECORD_UNITTEST=true
 #export APPMAP=false
-# export PYTHONPATH=$HOME/src/appmap-python
+#export PYTHONPATH=$HOME/src/appmap-python:$PWD/init
 APPMAP=true python3 -m unittest program_unittest.py
